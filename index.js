31e30b0e5b48893b2184d010f9db1bde62232285
José Iván Garita Lobato <58179973+Ivan-Garita99@users.noreply.github.com>
2024-10-05 00:50:41 -0600

    Update index.js
    Se agregan funciones nuevas de SweetAlert2

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const yesBtn = document.querySelector('#yesBtn');
 
-yesBtn.addEventListener('click', function () {
-    alert('Siuuu 🥳 sabia que dirias que si ')
-    confirm('Te apetece pizza?')
-    confirm('O hazme una seña de que se te antoja?')
+yesBtn.addEventListener('click', function (event) {
+    // Prevenir el envío del formulario
+    event.preventDefault();
+
+    // Mostrar el primer SweetAlert
+    Swal.fire({
+        title: 'Siuuu 🥳',
+        text: 'Sabía que dirías que sí!',
+        icon: 'success',
+        confirmButtonText: '¡Vamos!',
+        showClass: {
+            popup: 'animate__animated animate__fadeInUp animate__faster'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutDown animate__faster'
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Si el usuario confirma, muestra el siguiente SweetAlert
+            Swal.fire({
+                title: '¿Te parece si vamos al Vittorios?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Sí',
+                cancelButtonText: 'O, ¿qué te parece?' // Cambia el texto aquí
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('¡Genial! Nos vemos al salir. 🎉');
+                } else {
+                    Swal.fire('Si me dices que se te antoja cenar..');
+                }
+            });
+        }
+    });
 });
 
 const noBtn = document.querySelector('#noBtn');
@@ -14,5 +44,5 @@ noBtn.addEventListener('mouseover', function () {
     noBtn.style.setProperty('top', randomY + '%');
     noBtn.style.setProperty('left', randomX + '%');
     noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
-})
+});
 
